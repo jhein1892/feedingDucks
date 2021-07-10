2570e7ef0fab81b3031e59c006f0f4be45014cd0
Jacob Hein <jacobhein1892@gmail.com>
2021-07-09 21:04:07 -0700

    all basic functionality complete, moving into 'nice to haves'

--- frontend/src/components/Bottom.js
+++ frontend/src/components/Bottom.js
@@ -2,13 +2,32 @@ import React from 'react'
 import { makeStyles } from '@material-ui/core'
 import Feeding from './Feeding.js'
 const useStyles = makeStyles(() => ({
-
+  root:{
+    background:'linear-gradient(#6c6cff, #030369)',
+    paddingTop:'1%',
+    paddingBottom:'1%'
+  },
+  table:{
+    border: '1px black solid',
+    borderRadius: '10px',
+    width: '90%',
+    margin:'auto',
+    background:'white'
+  },
+  header:{
+    display:'flex',
+    justifyContent: 'space-around'
+  },
+  row:{
+    maxWidth:'15vw',
+    minWidth:'15vw'
+  }
 }))
 
 
 export default function Bottom(props){
   const { state } = props
-
+  const classes = useStyles('');
   const feedingInstances = state.map((e) => {
       return (
         <Feeding
@@ -26,9 +45,18 @@ export default function Bottom(props){
   // let pastFeedings = feedingInstances()
 
   return (
-    <div>
-      <h1>Bottom Half</h1>
-      {feedingInstances}
+    <div className={classes.root}>
+      <div className={classes.table}>
+        <div className={classes.header}>
+          <p className={classes.row}>Location</p>
+          <p className={classes.row}>Time Fed</p>
+          <p className={classes.row}>Type of Food</p>
+          <p className={classes.row}># of Ducks Fed</p>
+          <p className={classes.row}>Amount of Food / Duck</p>
+        </div>
+        <hr/>
+        {feedingInstances}
+      </div>
     </div>
   )
 }
--- frontend/src/components/Feeding.js
+++ frontend/src/components/Feeding.js
@@ -3,22 +3,29 @@ import { makeStyles } from '@material-ui/core'
 
 const useStyles = makeStyles (() => ({
   root:{
-    display:'flex'
+    display:'flex',
+    justifyContent: 'space-around',
+    '&:hover':{
+      background:'#0080004d'
+    }
+  },
+  row:{
+    maxWidth:'15vw',
+    minWidth:'15vw' 
   }
 }))
 
 export default function Feeding(props){
   const classes = useStyles('');
-  console.log('props', props)
   const { location, amount_fed, number_fed, type_of_food, time } = props
 
   return (
     <div className={classes.root}>
-      <p>{location}</p>
-      <p>{amount_fed}</p>
-      <p>{number_fed}</p>
-      <p>{type_of_food}</p>
-      <p>{time}</p>
+      <p className={classes.row}>{location}</p>
+      <p className={classes.row}>{time}</p>
+      <p className={classes.row}>{type_of_food}</p>
+      <p className={classes.row}>{amount_fed}</p>
+      <p className={classes.row}>{number_fed}</p>
     </div>
   )
 }
